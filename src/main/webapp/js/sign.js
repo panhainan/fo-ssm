0f13a2c19d608d03d9403fc89dfc8d00dd69a948
panhainan <phnlove@163.com>
2017-02-07 21:38:53 +0800

    登录注册验证

--- src/main/webapp/js/sign.js
+++ src/main/webapp/js/sign.js
@@ -2,8 +2,8 @@
  * Created by fo on 2017/2/5.
  */
 $(document).ready(function () {
+
     $('#signInForm').bootstrapValidator({
-        message: 'This value is not valid',
         feedbackIcons: {
             valid: 'glyphicon glyphicon-ok',
             invalid: 'glyphicon glyphicon-remove',
@@ -58,94 +58,103 @@ $(document).ready(function () {
                 if (!responseData.success) {
                     alert(responseData.errorMsg);
                 } else {
-                    //location.href = "index.html"
+                    location.href = "index.html"
                 }
             }
         });
     });
-    ;
-    /*$("#signInForm").submit(function(e) {
-     e.preventDefault();
-     var user = {
-     userName: $("#inputAccount").val(),
-     userPass: $("#inputPassword").val()
-     };
-     console.info(user);
-     $.ajax({
-     type: "POST",
-     url: "user/signIn",
-     data: user,
-     success: function (responseData) {
-     console.log(responseData);
-     if (!responseData.success) {
-     alert(responseData.errorMsg);
-     } else {
-     location.href = "index.html"
-     }
-     }
-     });
-     });*/
+
+    $('#signUpForm').bootstrapValidator({
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            'userName': {
+                validators: {
+                    notEmpty: {
+                        message: '用户名不能为空'
+                    },
+                    stringLength: {
+                        min: 3,
+                        max: 20,
+                        message: '长度必须在3-20之间'
+                    },
+                    regexp: {
+                        regexp: /^[a-zA-Z0-9_]+$/,
+                        message: '只能使用大小写字母、数字、下划线'
+                    }
+                }
+            },
+            userEmail: {
+                validators: {
+                    notEmpty: {
+                        message: '邮箱不能为空'
+                    },
+                    emailAddress: {
+                        message: '格式不正确'
+                    },
+                    stringLength: {
+                        min: 1,
+                        max: 20,
+                        message: '长度必须在1-20之间'
+                    }
+                }
+            },
+            userPass: {
+                validators: {
+                    notEmpty: {
+                        message: '密码不能为空'
+                    },
+                    stringLength: {
+                        min: 6,
+                        max: 16,
+                        message: '长度必须在6-16之间'
+                    }
+                }
+            },
+            userConfirmPass: {
+                validators: {
+                    notEmpty: {
+                        message: '密码不能为空'
+                    },
+                    identical: {
+                        field: 'userPass',
+                        message: '两次密码不一致'
+                    },
+                    stringLength: {
+                        min: 6,
+                        max: 16,
+                        message: '长度必须在6-16之间'
+                    }
+                }
+            }
+
+        }
+    }).on("success.form.bv", function (e) {
+        e.preventDefault();
+        var $form = $(e.target);
+        var validator = $form.data('bootstrapValidator');
+        var user = {
+            userName: validator.getFieldElements('userName').val(),
+            userPass: validator.getFieldElements('userPass').val(),
+            userEmail: validator.getFieldElements('userEmail').val()
+        };
+        console.info(user);
+        $.ajax({
+            type: "POST",
+            url: "user/signUp",
+            data: user,
+            success: function (responseData) {
+                console.log(responseData);
+                if (!responseData.success) {
+                    alert(responseData.errorMsg);
+                } else {
+                    location.href = "sign-in.html"
+                }
+            }
+        });
+    });
+
 });
-function checkAccount() {
-    var userName = $("#inputAccount").val();
-    $.ajax({
-        type: "POST",
-        url: "user/checkAccount",
-        data: {"userName": userName},
-        success: function (responseData) {
-            console.log(responseData);
-            if (responseData.success) {
-                if (!responseData.data.exist) {
-                    $("#accountDiv").addClass("has-success");
-                    return true;
-                } else {
-                    $("#accountDiv").addClass("has-error");
-                    return false;
-                }
-            }
-        }
-    });
-    return false;
-}
-function checkEmail() {
-    var userEmail = $("#inputEmail").val();
-    $.ajax({
-        type: "POST",
-        url: "user/checkEmail",
-        data: {"userEmail": userEmail},
-        success: function (responseData) {
-            console.log(responseData);
-            if (responseData.success) {
-                if (!responseData.data.exist) {
-                    $("#emailDiv").addClass("has-success");
-                    return true;
-                } else {
-                    $("#emailDiv").addClass("has-error");
-                    return false;
-                }
-            }
-        }
-    });
-    return false;
-}
-function signUp() {
-    var user = {
-        userName: $("#inputAccount").val(),
-        userPass: $("#inputPassword").val(),
-        userEmail: $("#inputEmail").val()
-    };
-    console.info(user);
-    $.ajax({
-        type: "POST",
-        url: "user/signUp",
-        data: user,
-        success: function (responseData) {
-            console.log(responseData);
-            if (!responseData.success) {
-                alert(responseData.errorMsg);
-            } else {
-                location.href = "sign-in.html"
-            }
-        }
-    });
-}
